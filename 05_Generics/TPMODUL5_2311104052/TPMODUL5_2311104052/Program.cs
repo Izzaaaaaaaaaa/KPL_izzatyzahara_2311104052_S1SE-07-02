6f83348dfb4ab3049c77fcf68ee765ec2624af21
izzaaaaaaaaaa <izzatyzahara2424@gmail.com>
2025-03-11 11:30:26 +0700

    create folder

--- 05_Generics/TPMODUL5_2311104052/TPMODUL5_2311104052/Program.cs
+++ 05_Generics/TPMODUL5_2311104052/TPMODUL5_2311104052/Program.cs
@@ -1,2 +1,20 @@
-﻿Console.WriteLine("haloo");
-Console.ReadLine();
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T nama)
+    {
+        Console.WriteLine($"Halo user {nama}!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Izzaty"); // Ganti dengan nama panggilanmu
+
+        Console.ReadLine(); // Agar console tidak langsung tertutup
+    }
+}
