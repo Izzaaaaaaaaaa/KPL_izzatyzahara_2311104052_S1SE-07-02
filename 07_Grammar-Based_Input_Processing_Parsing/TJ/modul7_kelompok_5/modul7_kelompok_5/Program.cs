a3e8faddc5f4239a03a5fda0057d13246c69d4cf
izzaaaaaaaaaa <izzatyzahara2424@gmail.com>
2025-04-17 14:47:32 +0700

    menambahkan class DataMahasiswa2311104052

---
+++ 07_Grammar-Based_Input_Processing_Parsing/TJ/modul7_kelompok_5/modul7_kelompok_5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using modul7_kelompok_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== [1] Menampilkan Parsing Data Mahasiswa ===");
+        var dataMahasiswa = new DataMahasiswa2311104052();
+        dataMahasiswa.ReadJSON();
+        Console.ReadLine();
+    }
+}
