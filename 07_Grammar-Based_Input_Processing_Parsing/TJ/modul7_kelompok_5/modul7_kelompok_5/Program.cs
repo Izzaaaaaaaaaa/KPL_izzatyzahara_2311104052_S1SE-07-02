0fb5d86dd10d14ec46d6ead05232128df11bfeea
izzaaaaaaaaaa <izzatyzahara2424@gmail.com>
2025-04-17 14:00:29 +0700

    menambahkan class DataMahasiswa2311104052

--- 07_Grammar-Based_Input_Processing_Parsing/TJ/modul7_kelompok_5/modul7_kelompok_5/Program.cs
+++ 07_Grammar-Based_Input_Processing_Parsing/TJ/modul7_kelompok_5/modul7_kelompok_5/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using modul7_kelompok_5;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== [1] Menampilkan Parsing Data Mahasiswa ===");
+        var datamahasiswa = new DataMahasiswa2311104052();
+        datamahasiswa.ReadJSON();
+        Console.ReadLine();
+    }
+}
--- 07_Grammar-Based_Input_Processing_Parsing/TJ/modul7_kelompok_5/modul7_kelompok_5/obj/Release/net8.0/modul7_kelompok_5.AssemblyInfo.cs
+++ 07_Grammar-Based_Input_Processing_Parsing/TJ/modul7_kelompok_5/modul7_kelompok_5/obj/Release/net8.0/modul7_kelompok_5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul7_kelompok_5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9695e8bc943f6bca332fdc0f6412e7eb0ec1f1e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8165a966a9a44e40d3f662edab4e1151275cfe8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul7_kelompok_5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul7_kelompok_5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
