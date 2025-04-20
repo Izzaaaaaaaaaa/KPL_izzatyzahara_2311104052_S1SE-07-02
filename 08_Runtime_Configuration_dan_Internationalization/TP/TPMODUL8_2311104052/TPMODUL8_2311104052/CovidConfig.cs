aab3b9ca93a5934828634c69afe534bd3f866bfc
izzaaaaaaaaaa <izzatyzahara2424@gmail.com>
2025-04-20 13:23:33 +0700

    menyelesaikan program dengan runtime configuration

---
+++ 08_Runtime_Configuration_dan_Internationalization/TP/TPMODUL8_2311104052/TPMODUL8_2311104052/CovidConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class CovidConfig
+{
+    public string satuan_suhu { get; set; }
+    public int batasan_hari_demam { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+
+    public static string filePath = "covid_config.json";
+    internal int batas_hari_deman;
+
+    public CovidConfig()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            var config = JsonSerializer.Deserialize<CovidConfig>(json);
+            satuan_suhu = config.satuan_suhu;
+            batasan_hari_demam = config.batasan_hari_demam;
+            pesan_ditolak = config.pesan_ditolak;
+            pesan_diterima = config.pesan_diterima;
+        }
+        else
+        {
+            satuan_suhu = "celcius";
+            batasan_hari_demam = 14;
+            pesan_ditolak = "Anda tidak di perbolehkan masuk ke dalam gedung ini";
+            pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+            SaveConfig();
+
+        }
+    }
+
+    public void UbahSatuan()
+    {
+        if (satuan_suhu.ToLower() == "celcius")
+        {
+            satuan_suhu = "fahrenheit";
+        }
+        else
+        {
+            satuan_suhu = "celcius";
+        }
+        SaveConfig();
+    }
+    public void SaveConfig()
+    {
+        var json = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(filePath, json);
+    }
+}
---
+++ 08_Runtime_Configuration_dan_Internationalization/TP/TPMODUL8_2311104052/TPMODUL8_2311104052/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = new CovidConfig();
+
+        Console.WriteLine("== Program Cek Suhu & Riwayat Demam ==");
+
+        // Menampilkan input pertama
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        // Input kedua
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuNormal = false;
+
+        if (config.satuan_suhu == "celcius")
+        {
+            suhuNormal = suhu >= 36.5 && suhu <= 37.5;
+        }
+        else if (config.satuan_suhu == "fahrenheit")
+        {
+            suhuNormal = suhu >= 97.7 && suhu <= 99.5;
+        }
+
+        if (suhuNormal && hariDemam < config.batas_hari_deman)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+
+        // Contoh penggunaan method UbahSatuan
+        Console.Write("\nIngin mengganti satuan suhu? (y/n): ");
+        string pilihan = Console.ReadLine();
+        if (pilihan.ToLower() == "y")
+        {
+            config.UbahSatuan();
+            Console.WriteLine($"Satuan suhu sekarang adalah: {config.satuan_suhu}");
+            Console.ReadLine();
+        }
+    }
+}
