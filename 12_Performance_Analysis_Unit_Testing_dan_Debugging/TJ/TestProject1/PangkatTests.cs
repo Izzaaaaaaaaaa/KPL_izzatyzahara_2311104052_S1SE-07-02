5ba1c9ed50051608a37630d313449df331713e3c
izzaaaaaaaaaa <izzatyzahara2424@gmail.com>
2025-05-15 14:44:03 +0700

    chore: finalize Modul 12 implementation and unit testing for power calculator

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/TJ/TestProject1/PangkatTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TJMod12_2311104052;
+
+[TestClass]
+public class PangkatTests
+{
+    [TestMethod]
+    public void TestPangkatB0()
+    {
+        Assert.AreEqual(1, Form1.CariNilaiPangkat(0, 0));
+    }
+
+    [TestMethod]
+    public void TestPangkatNegatif()
+    {
+        Assert.AreEqual(-1, Form1.CariNilaiPangkat(5, -2));
+    }
+
+    [TestMethod]
+    public void TestBesarDari10()
+    {
+        Assert.AreEqual(-2, Form1.CariNilaiPangkat(5, 11));
+    }
+
+    [TestMethod]
+    public void TestLebih100()
+    {
+        Assert.AreEqual(-2, Form1.CariNilaiPangkat(101, 3));
+    }
+
+    [TestMethod]
+    public void TestOverflow()
+    {
+        Assert.AreEqual(-3, Form1.CariNilaiPangkat(9, 30));
+    }
+
+    [TestMethod]
+    public void TestNormal()
+    {
+        Assert.AreEqual(27, Form1.CariNilaiPangkat(3, 3));
+    }
+}
