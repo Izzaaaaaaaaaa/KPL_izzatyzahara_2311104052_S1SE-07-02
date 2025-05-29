c3cfc3e9579852cff85152166cef9ae388dd170a
izzaaaaaaaaaa <izzatyzahara2424@gmail.com>
2025-05-29 22:07:04 +0700

    Refactor code untuk meningkatkan clean code dan penanganan error

---
+++ 14_Clean_Code/TJ/TJmod14_2311104052/TJmod14_2311104052/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using PusatDataSingleton;
+
+class Program
+{
+    static void Main()
+    {
+        var pusat = PusatData.GetInstance();
+
+        pusat.TambahkanData("Andi");
+        pusat.TambahkanData("Budi");
+        pusat.TambahkanData("Rian");
+
+        Console.WriteLine("Data awal:");
+        pusat.PrintSemuaData();
+
+        pusat.HapusSebuahData(2); // hapus "Rian"
+
+        Console.WriteLine("\nData setelah penghapusan:");
+        pusat.PrintSemuaData();
+
+        Console.ReadKey();
+    }
+}
